b6cd59b0db6a4a8e1bc31cb033b0cef3fda58708
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-01-17 19:41:21 +0100

    man kan logga in, datan sparas i databasen på mongoatlas, f

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const express = require('express') 
 const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express()  //app är ett variabel namn för express
+
+//DB CONFIG
+
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db,{ useNewUrlParser: true ,useUnifiedTopology: true})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
 
 //EJS
 
 app.use(expressLayouts);
-app.set('view engine', 'ejs') 
+app.set('view engine', 'ejs') ;
 
+//Bodyparser
+
+app.use(express.urlencoded({ extended: false }));
+
+//Express session
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+//connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next)=> {
+  res.locals.success_msg = req.flash('success_msg');
+  
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 // routes
 
-app.use('/', require('./routes/index'))
-//console.log("hej")
-app.use('/users', require('./routes/users'))
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 5000;
  
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports ={
+
+MongoURI: 'mongodb+srv://viktorliljenberg:Zohan0109@cluster0.b0bo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+}

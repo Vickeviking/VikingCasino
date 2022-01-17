08dc44d33fea7e5c218ad99fa8852a48e304706c
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-01-17 20:52:52 +0100

    Det funkar nuuu

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 const app = express()  //app är ett variabel namn för express
+
+//Passport config
+
+require('./config/passport')(passport);
 
 //DB CONFIG
 
@@ -33,6 +38,11 @@ app.use(session({
   saveUninitialized: true,
 }))
 
+//passport middleware
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect flash
 app.use(flash());
 
@@ -41,6 +51,10 @@ app.use((req, res, next)=> {
   res.locals.success_msg = req.flash('success_msg');
   
   res.locals.error_msg = req.flash('error_msg');
+
+  
+  res.locals.error = req.flash('error');
+
   next();
 });
 

785760f08f3d8aba309fedb767bfabb87dc16031
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-01-21 12:39:36 +0100

    dealer sum m.m.

--- public/js/blackJack.js
+++ public/js/blackJack.js
@@ -1,10 +1,11 @@
 //black jack code
-let player= {
-    name: "",
-    chips: 100
-}
- 
+let player = {
+  name: "",
+  chips: 100,
+};
+
 let cards = [];
+let dealerCards = [];
 let sum = 0;
 let hasBlackJack = false;
 let isAlive = false;
@@ -15,28 +16,26 @@ let clickMessageEl = document.getElementById("clickMessage-el");
 let sumEl = document.querySelector("#sum-el");
 let messageEL = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
+let dealerCardsEl = document.getElementById("dealerCards-el");
 let stoppa = false;
-
- 
- 
- 
+let cardSet = false;
+let firstDCard = 0;
+let secondDCard = 0;
+let over17 = false;
 let playerEl = document.getElementById("player-el");
 playerEl.textContent = player.name + ": $" + player.chips;
 
+clickMessageEl.textContent = "Start";
 
-    clickMessageEl.textContent = "Start"
-
-
- 
-function startGame(){
-    if(player.chips > 0){
-        player.chips = player.chips - 5;
+function startGame() {
+  if (stoppa == false) {
+    if (player.chips > 0) {
+      player.chips = player.chips - 5;
     }
-        skickaVidare();
+    skickaVidare();
+  }
 
-}
-
-function skickaVidare(){
+  function skickaVidare() {
     cards = [];
     hasBlackJack = false;
     dead = false;
@@ -47,70 +46,108 @@ function skickaVidare(){
     cards = [firstCard, secondCard]; //ordered list of items
     sum = firstCard + secondCard;
     renderGame();
-
+  }
 }
 
-function renderGame(){
-    playerEl.textContent = player.name + ": $" + player.chips;
-    cardsEl.textContent = "Cards: "
- 
-    for(let i = 0; i < cards.length; i++ ){
- 
-        cardsEl.textContent += cards[i] + " ";
-    }
- 
- 
-    sumEl.textContent = "Sum: " + sum;
- 
-        if(sum <= 20)
-    {
-        message = "Would you like to draw again?";
-    }else if(sum === 21)
-    {
-        message = "You've got Black Jack!";
-        hasBlackJack = true;
-        isAlive = false;
-    } else {
-        message = "You're out of the game";
-        isAlive = false;
-    }
-    messageEL.textContent = message;
+function renderGame() {
+  playerEl.textContent = player.name + ": $" + player.chips;
+  cardsEl.textContent = "Cards: ";
 
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+
+  if (sum <= 20) {
+    message = "Would you like to draw again?";
+  } else if (sum === 21) {
+    message = "You've got Black Jack!";
+    hasBlackJack = true;
+    isAlive = false;
+  } else {
+    message = "You're out of the game";
+    isAlive = false;
+  }
+  messageEL.textContent = message;
 }
- 
-function newCard()
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false && stoppa === false) {
+    let card = getRandomCard();
+    sum += card;
+
+    cards.push(card);
+
+    renderGame();
+  }
+}
+
+function stoppar() {
+  stoppa = true;
+  dealerGive();
+}
+
+function dealerGive() {
+  dealerCards = [];
+
+  if(cardSet == false){
+   firstDCard = getRandomCard();
+   secondDCard = getRandomCard();
+  dealerSum = firstDCard + secondDCard;
+}
+console.log(dealerSum);
+ cardSet = true;
+
+  dealerCards = [firstDCard, secondDCard];
+  dealerCardsEl.textContent = "DealerCards: " + dealerSum;
+ /* for (let i = 0; i < dealerCards.length; i++) {
+    dealerCardsEl.textContent += dealerCards[i] + " ";
+  }*/
+
+  if(dealerSum < 17){
+      dealerGiveAnother();
+  } else{
+      dealerDone();
+  }
+  
+}
+
+function dealerGiveAnother()
 {
-    if(isAlive === true && hasBlackJack === false)
-    {
- 
-        let card = getRandomCard();
-        sum += card;
-   
-        cards.push(card);
-   
-        renderGame();
-    }
- 
- 
+    console.log("dealer wants new card");
+    let newDealerCard = getRandomCard();
+    console.log(newDealerCard);
+    dealerCards.push(newDealerCard);
+    console.log(dealerCards);
+dealerSum = dealerSum + newDealerCard;
+    dealerGive();
+
+    /*dealerSum = newDealerCard + dealerSum;
+
+    dealerCards.push(newDealerCard);
+
+    dealerGive();*/
+
+
 }
 
-function stoppar()
+function dealerDone()
 {
-    console.log("Stannar");
- 
+    console.log("färdig");
 }
 
 
 
- 
-function getRandomCard()
-{
-    let randomNumber =  Math.floor(Math.random()*13) + 1;
-    if (randomNumber > 10){
-        return 10;
-    } else if (randomNumber === 1){
-            return 11;
-    } else {
-        return randomNumber;
-    }
+
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }

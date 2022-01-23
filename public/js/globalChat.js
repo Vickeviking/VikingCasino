c4830b17c2cff07feb832d7ec5b61c1cee294a96
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-01-23 20:08:24 +0100

    hejsan

--- public/js/globalChat.js
+++ public/js/globalChat.js
@@ -1,32 +1,40 @@
 let sms1 = document.querySelector("#msg-1");
 let babe = document.getElementById("msg-2");
+let babe1 = document.getElementById("msg-3");
+let babe2 = document.getElementById("msg-4");
+let firstT = true;
 
-function sleep(miliseconds) {
-    var currentTime = new Date().getTime();
-    while (currentTime + miliseconds >= new Date().getTime()) {
-    }
-}
 
 function fn1(){
 
+    if(firstT === true){
   
    console.log("körs");
    
     var medelande = document.getElementById("message-input").value;
     sms1.textContent = "You: " + medelande;
-    babey();
     
+    setTimeout(function () {
+        babe.textContent = "- babeRexa joined the chat";
+    }, 2000);
+    
+    setTimeout(function () {
+        babe1.textContent = "babeRexa: if you spend 20$ at the blackjack right now, im gonna do whatever it takes to please you baby";
+    }, 3000);
+
+    setTimeout(function () {
+        babe2.textContent = "babeRexa: When i say anything i really mean it ;)";
+    }, 4500);
+
+    firstT = false;
+
+    }else {
+        alert("Our girls don't work for free, spend more money to continue!");
+    }
 }
 
-function babey(){
 
 
-
-
-    
-    sleep(1000);
-    babe.textContent = "- babeRexa joined the chat";
-}
 
 
    

c0917cc7699840aa4ebb507daf54d9c46d3eb153
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-01-24 18:12:46 +0100

    store fixat

--- models/User.js
+++ models/User.js
@@ -31,15 +31,42 @@ date: {
 
 const User = mongoose.model('User', UserSchema);
 
+  User.findOneAndUpdate({email: 'text' }, {coins: 500 }, (error, data) => {
+    if(error){
+      console.log(error);
+    }else{
+      console.log(data);
+    }
+  })  
 
 
-User.findOneAndUpdate({email: 'viktorlilienberg@gmail.com' }, {coins: 200 }, (error, data) => {
-  if(error){
-    console.log(error);
-  }else{
-    console.log(data);
-  }
-})  
+
+function buy2000(){
+
+  User.findOneAndUpdate({email: 'viktorlilienberg@gmail.com' }, {coins: coins + 2000 }, (error, data) => {
+    if(error){
+      console.log(error);
+    }else{
+      console.log(data);
+    }
+  })  
+}
+
+
+
+function buy5000(){
+
+  User.findOneAndUpdate({email: 'viktorlilienberg@gmail.com' }, {coins: coins + 5000 }, (error, data) => {
+    if(error){
+      console.log(error);
+    }else{
+      console.log(data);
+    }
+  })  
+}
+
+
+
 
 
 module.exports = User; 
